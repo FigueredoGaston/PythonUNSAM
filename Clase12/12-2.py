36c2958ea77890ecf6d62abd7568631fb0ed18c4
FigueredoGaston <figueredogaston@gmail.com>
2021-10-30 17:59:23 -0300

    30-10

---
+++ Clase12/12-2.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Ejercicio 11.9: Pascal
+
+def ord_burbujeo(lista):
+    return []
+
+
+if __name__ == '__main__':
+    print(ord_burbujeo([1, 2, -3, 8, 1, 5]))
